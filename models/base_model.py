6b2cdb388061c3d3582deac8cbb805e2a82f9849
asandelarvine <asandelarvine@gmail.com>
2022-09-04 22:13:03 +0300

    save method that updates the public instance attribute updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,13 @@ class BaseModel:
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    
+    def save(self):
+        '''updates the public instance attribute updated_at
+        with the current datetime
+        '''
+
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''
