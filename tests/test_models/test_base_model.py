5b4ff3dcbbb8e24ccc8342677aaf7b0bfcaa8e3b
asandelarvine <asandelarvine@gmail.com>
2022-09-04 13:04:47 +0300

    test for updated_at

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,3 +15,14 @@ class TestBaseModel(unittest.TestCase):
         my_model = BaseModel()
         date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         self.assertEqual(my_model.created_at.strftime("%Y-%m-%dT%H:%M:%S"), date)
+
+    def test_updated_at(self):
+        try:
+            my_model = BaseModel()
+            updated_at = my_model.updated_at
+            my_model.save()
+
+            self.assertNotEqual(updated_at, my_model.updated_at), \
+                    "updated_at not changeing after new modification"
+        except AssertionError as e:
+            print(e)
